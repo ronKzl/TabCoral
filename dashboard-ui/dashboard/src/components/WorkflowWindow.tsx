8bbd73f63fab7fe26e4249ae690812f44f7097de
Ben Dover <ron.stuchevsky@gmail.com>
2025-06-20 14:22:39 -0400

    set up redux hooks with typescript

--- dashboard-ui/dashboard/src/components/WorkflowWindow.tsx
+++ dashboard-ui/dashboard/src/components/WorkflowWindow.tsx
@@ -1,18 +1,35 @@
 import Box from '@mui/material/Box';
-import fs from 'fs';
+// import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import {useAppSelector} from '../hooks'
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
-
-function WorkflowWindow() {
+function WorkflowWindow() {   
     
-    function exportData() {
-      
-    }
+    const tabs = useAppSelector((state) => state.workflows)
+    console.log(tabs)
     return (
-        <Box>
-            workflow window
-
-        </Box>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        {Array.from(Array(6)).map((_, index) => (
+          <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+            <Item>{index + 1}</Item>
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
     )
 }
 
