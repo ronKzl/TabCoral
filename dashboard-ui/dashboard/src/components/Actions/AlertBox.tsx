897f7ac5809b7e062e304929a80b921b3fb78e6d
RonKzl <ron.stuchevsky@gmail.com>
2025-07-03 17:16:23 -0400

    added action dialog popup component to prompt user to verify their deletion actions

---
+++ dashboard-ui/dashboard/src/components/Actions/AlertBox.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+interface AlertDialogProps {
+    open: boolean,
+    close: any,
+    title: string,
+    content?: string
+}
+
+export default function AlertDialog({title, open, close, content}: AlertDialogProps) {
+  
+  return (
+    <React.Fragment>
+      <Dialog
+        open={open}
+        onClose={close}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {title}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {content}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={close} autoFocus>No</Button>
+          <Button onClick={close}>
+            Yes
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </React.Fragment>
+  );
+}
--- dashboard-ui/dashboard/src/components/TabCard.tsx
+++ dashboard-ui/dashboard/src/components/TabCard.tsx
@@ -1,74 +1,101 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import CardMedia from '@mui/material/CardMedia';
-import {type tab} from "../interfaces/session"
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import CardMedia from "@mui/material/CardMedia";
+import { type tab } from "../interfaces/session";
+import AlertDialog from "./Actions/AlertBox";
 
-
-function TabCard( {favicon, url, title, index, id} : tab){
-      
-
+function TabCard({ favicon, url, title, index, id }: tab) {
   const handleTabOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log("here tab is opening up")
-    chrome.tabs.create({active: true, index: index, url: url})
+    chrome.tabs.create({ active: true, index: index, url: url });
     event.stopPropagation();
   };
 
-  async function handleTabRemoval(event: React.MouseEvent<HTMLButtonElement>) {
-    //popup are you sure window
-    
-    //if its open in the session close it
-    let result = await chrome.tabs.get(id)
-    if (result !== undefined){
-      chrome.tabs.remove(id);
-    }
+  const [isDialogOpen, setOpen] = React.useState(false);
 
-    //remove from database
-    
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
+  const handleClose = () => {
+    console.log("I am closing");
+    setOpen(false);
+  };
 
-    event.stopPropagation();
+  //event: React.MouseEvent<HTMLButtonElement>
+  async function handleTabRemoval() {
+    //popup are you sure window - redo with MUI
+    handleClickOpen();
+    console.log(id);
+    // let text = `Remove ${title} from your current saved session?\nThis opeartion can not be undone.`;
+    // if (confirm(text) == true) {
+    //   let result = await chrome.tabs.get(id);
+    //   if (result !== undefined) {
+    //     chrome.tabs.remove(id);
+    //   }
+    //   //remove from database
+
+    //   //show snackbar
+
+    // }
+    //show abort snackbar
   }
 
-const card = (
-  
-  <React.Fragment>
-    <CardContent sx = {{maxHeight: 150}}>
-      <Typography gutterBottom sx={{ color: 'text.secondary', fontSize: 14 }}>
-        <CardMedia
-        component="img"
-        alt={title}
-        height="16"
-        image={favicon}
-        sx={{ maxWidth: 16 ,
-          backgroundColor: 'grey', // light gray background #f0f0f0
-          borderRadius: '4px',
-          padding: '2px',
-          border: '2px solid #ccc' }}
-      />
-      </Typography>
-      <Typography noWrap variant="h5" component="div">
-        <a href={url}>{title}</a>
-      </Typography>
-    </CardContent>
-    <CardActions>
-      <Button size="small" sx={{color: 'green'}} onClick={handleTabOpen}>Open Tab</Button>
-      <Button size="small" sx={{color: 'red'}} onClick={handleTabRemoval}>Remove From Session</Button>
-    </CardActions>
-  </React.Fragment>
-);
+  const card = (
+    <React.Fragment>
+      <CardContent sx={{ maxHeight: 150 }}>
+        <Typography gutterBottom sx={{ color: "text.secondary", fontSize: 14 }}>
+          <CardMedia
+            component="img"
+            alt={title}
+            height="16"
+            image={favicon}
+            sx={{
+              maxWidth: 16,
+              backgroundColor: "grey", // light gray background #f0f0f0
+              borderRadius: "4px",
+              padding: "2px",
+              border: "2px solid #ccc",
+            }}
+          />
+        </Typography>
+        <Typography noWrap variant="h5" component="div">
+          <a href={url}>{title}</a>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" sx={{ color: "green" }} onClick={handleTabOpen}>
+          Open Tab
+        </Button>
+        <Button
+          size="small"
+          sx={{ color: "red" }}
+          onClick={(e) => {
+            handleTabRemoval(), e.stopPropagation();
+          }}
+        >
+          Remove From Session
+        </Button>
+      </CardActions>
+    </React.Fragment>
+  );
 
-
-    return (
+  return (
     <Box sx={{ minWidth: 275 }}>
       <Card variant="outlined">{card}</Card>
       <br />
+      <AlertDialog
+        open={isDialogOpen}
+        close={() => handleClose()}
+        title={`Remove ${title} from your current saved session?\n`}
+        content={"This action can't be undone."}
+      />
     </Box>
-  )
+  );
 }
 
 export default TabCard;
